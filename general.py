07fb7db862d33a4297ab18addd5c594200a4592e
Shreyal Shah <shreyalshah2014@gmail.com>
2017-12-27 23:06:30 -0500

    Created linkfinder and read through html code, stored it in sets

--- general.py
+++ general.py
@@ -25,9 +25,30 @@ def write_file(path, data):
     
 create_data_files('project', 'https://typicalmitul.com')
 
+## add data onto an existing file
+## call whenever you want to add a new link to the end of the file
+def append_to_file(path, data):
+    with open(path, 'a') as file:   # a means append
+        file.write(data + \n)
 
+# Delete the contents of the file
+def delete_file_contents(path):
+    with open(path, 'w'):
+        pass 
 
+## Store contents as a 'set' cuz we dont want duplication 
 
+# Read a file and convert each line to set items 
+def file_to_set(file_name):
+    results = set()
+    with open(file_name, 'rt') as f:
+        for line in f:
+            results.add(line.replace('\n',''))
 
+# Iterate through each set, each item will be a new line in the file 
+def set_to_file(links, file):
+    delete_file_contents(file)
+    for link in sorted(links):
+        append_to_file(file, link)
 
 
---
+++ link_finder.py
@@ -0,0 +1,35 @@
+# Generic HTML parser to sift through website code ie read the website (HTML) code
+from html.parser import HTMLParser 
+from urllib import parse 
+
+# Make a class with HTMLParser functions along with other custom functions 
+class LinkFinder(HTMLParser):
+    
+    def __init__(self, base_url, page_url):
+        super().__init__()
+        self.base_url=base_url
+        self.page_url=page_url
+        self.links = set()
+
+    def handle_starttag(self, tag, attrs):
+        #if its an anchor tag 'a' loops through attributes to check if href
+        #if href, parse it, join the base url to it and store it to the set selflinks
+        if tag=='a':
+            for (attribute, value) in attrs:
+                if attribute=='href':
+                    url=parse.urljoin(self.base_url, value)
+                    self.links.add(url)
+
+# REturns the set with the links
+    def page_links(self):
+        return self.links
+
+    # Catch any errors and deliver the message
+    def error(self, message):
+        pass
+
+finder = LinkFinder()
+finder.feed('<html><head><title>Test</title></head>'
+            '<body><h1>Parse me</h1></body></html>')
+
+
