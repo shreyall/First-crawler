2e472025ff2fc6cdb25195a3ca94b4f86b6f52c1
Shreyal Shah <shreyalshah2014@gmail.com>
2017-12-27 19:21:19 -0500

    Created new file function and other functions

--- general.py
+++ general.py
@@ -4,5 +4,24 @@ def create_project_dir(directory):
         print('Creating prtoject' + directory)
         os.makedirs(directory)
 
-create_project_dir('First Project!')
+ # create_project_dir('First Project!')
+def create_data_files(projectname, baseurl):
+    queue=projectname + '/queue.txt' 
+    crawled = projectname + '/crawled.txt'
+    #if cue file exists, dont create, vice versa
+    # it cannot be empty cuz it will not know where to start
+    if not os.path.isfile(queue):
+        write_file(queue, baseurl)
+    if not os.path.isfile(crawled):
+        write_file(crawled, ' ')
+
+## create a new file
+def write_file(path,data):
+    f=open(path, 'w')
+    f.write(data)
+    f.close()
+
+
+
+
 
